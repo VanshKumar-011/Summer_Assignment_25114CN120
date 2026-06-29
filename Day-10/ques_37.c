0124402d0bc86fb882b2fe9c658c358d69f7f4f7
VanshKumar-011 <vanshkumar.ix@gmail.com>
2026-06-29 20:04:13 +0530

    Add files via upload

---
+++ Day-10/ques_37.c
@@ -0,0 +1,25 @@
+// Write a program to Print star pyramid
+
+#include<stdio.h>
+int main()
+{
+    int i,j,k,n;
+    printf("Enter The Number Of Rows");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        for(k=1;k<=n-i;k++)
+        {
+            printf(" ");
+        }
+
+        for(j=1;j<=2*i-1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+
+}
