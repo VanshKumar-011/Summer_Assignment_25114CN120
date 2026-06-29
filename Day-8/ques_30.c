963ad78600b9e0634f3f6c6f198399e8eec47c25
VanshKumar-011 <vanshkumar.ix@gmail.com>
2026-06-29 20:02:52 +0530

    Add files via upload

---
+++ Day-8/ques_30.c
@@ -0,0 +1,19 @@
+// Write a program to Print number triangle
+
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter The Number Of Rows:");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
